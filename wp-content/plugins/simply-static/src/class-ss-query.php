10eb609f1822d8a4b2827ee412faa50dd741cb0a
HypeXDigital <hello@hypesrilanka.com>
2023-02-24 03:15:00 +0530

    First Push

---
+++ wp-content/plugins/simply-static/src/class-ss-query.php
@@ -0,0 +1,2 @@
+<br>
+<b>Deprecated</b>:  Using ${var} in strings is deprecated, use {$var} instead in <b>C:\xampp\htdocs\HypeXEmailer\wp-content\plugins\simply-static\src\class-ss-query.php</b> on line <b>393</b><br>
